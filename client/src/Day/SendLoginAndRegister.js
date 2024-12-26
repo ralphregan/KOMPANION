85487399ea976167ed506597f6561b1fa12cbbc1
reganralph <regan_ralph@yahoo.com>
2024-12-26 18:59:42 -0500

    redirect

--- client/src/Day/SendLoginAndRegister.js
+++ client/src/Day/SendLoginAndRegister.js
@@ -2,6 +2,10 @@ import { useEffect, useRef} from "react";
 import {  useNavigate } from "react-router-dom";
 import axios from "axios"
 
+const api = axios.create({
+    baseURL: 'https://server-jdh7.onrender.com',
+    withCredentials: true // if you're using cookies
+})
 export default function SendRegister({Data, dispatch, hasSent}) {
  const navigate = useNavigate()
     const initialMount = useRef(true)
@@ -19,16 +23,16 @@ if(!Data || hasSent) {
 
 const controller = new AbortController()
 async function send() {
-    const api = Data.login ? '/Login' : '/Register'
+    const endpoint = Data.login ? '/Login' : '/Register'
     console.log(Data)
     try {
         console.log("started")
             dispatch({type: "SEND_START"})
-    const response = await axios.post(api, Data)
+    const response = await api.post(endpoint, Data)
     const result = response.data 
     console.log(result)
     dispatch({ type: "SEND_SUCCESS", payload: result });
-    if(Data.login) navigate("/day")
+    if(Data.login) navigate("/day",{replace: true})
 } catch (error) {
     console.log("there is an err", error)
  
